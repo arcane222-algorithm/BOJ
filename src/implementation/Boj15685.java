3a9da0bd9f255be6b40d7565824983e4efc2e04a
Lee Hong Jun <hong3883@naver.com>
2022-06-21 19:01:54 +0900

    Feat: Initialization commit (add solved boj problems)
    - Added solved boj problems
      (Form : boj(problem number).java)
      (Folders will be used to organize each problem into categories)

---
+++ src/implementation/Boj15685.java
@@ -0,0 +1,153 @@
+package implementation;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 드래곤 커브 - BOJ 15685
+ * -----------------
+ * Input 1
+ * 3
+ * 3 3 0 1
+ * 4 2 1 3
+ * 4 2 2 1
+ *
+ * Output 1
+ * 4
+ * -----------------
+ * Input 2
+ * 4
+ * 50 50 0 10
+ * 50 50 1 10
+ * 50 50 2 10
+ * 50 50 3 10
+ *
+ * Output 2
+ * 1992
+ * -----------------
+ */
+public class Boj15685 {
+
+    private static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Point{");
+            sb.append("x=");
+            sb.append(x);
+            sb.append(", y=");
+            sb.append(y);
+            sb.append('}');
+
+            return sb.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Point p = (Point) obj;
+            return (x == p.x) && (y == p.y);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    static final int[] dirX = {1, 0, -1, 0};
+    static final int[] dirY = {0, -1, 0, 1};
+    static final int MAP_SIZE = 101;
+
+    static int N;
+    static int x, y, d, g;
+
+    public static Point rotate(Point point, Point pivot) {
+        int x = -point.y + pivot.x + pivot.y;
+        int y = point.x - pivot.x + pivot.y;
+        return new Point(x, y);
+    }
+
+    public static void processGeneration(int[][] grid, int x, int y, int d, int g) {
+        Point pivot, pivotTmp = null;
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(x, y));
+        points.add(new Point(x + dirX[d], y + dirY[d]));
+        pivot = points.get(points.size() - 1);
+
+        for(int i = 0; i < g; i++) {
+            final int len = points.size();
+
+            for(int j = 0; j < len; j++) {
+                Point p = points.get(j);
+                Point r = rotate(p, pivot);
+                points.add(r);
+                if(p.x == x && p.y == y) {
+                    pivotTmp = r;
+                }
+            }
+            pivot = pivotTmp;
+        }
+
+        for(int i = 0; i < points.size(); i++) {
+            Point p = points.get(i);
+            grid[p.y][p.x] = 1;
+        }
+    }
+
+    public static int countSquare(int[][] grid) {
+        int cnt = 0;
+        for(int i = 0; i < MAP_SIZE - 1; i++) {
+            for(int j = 0; j < MAP_SIZE - 1; j++) {
+                if(grid[j][i] != 1) continue;
+                if(grid[j + 1][i] != 1) continue;
+                if(grid[j][i + 1] != 1) continue;
+                if(grid[j + 1][i + 1] != 1) continue;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void dumpGrid(int[][] grid) {
+        for(int i = 0; i < grid.length; i++) {
+            System.out.println(Arrays.toString(grid[i]));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int[][] grid = new int[MAP_SIZE][MAP_SIZE];
+
+        // parse N
+        N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            // parse x, y, d, g
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            d = Integer.parseInt(st.nextToken());
+            g = Integer.parseInt(st.nextToken());
+
+            // Process dragon curve's generation
+            processGeneration(grid, x, y, d, g);
+        }
+
+        // write the answer
+        int answer = countSquare(grid);
+        bw.write(String.valueOf(answer));
+
+        // close the buffer
+        br.close();
+        bw.close();
+    }
+}
---
+++ src/implementation/Boj17144.java
@@ -0,0 +1,185 @@
+package implementation;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 미세먼지 안녕! - BOJ 17144
+ * --------------------
+ * Input 1
+ * 7 8 3
+ * 0 0 0 0 0 0 0 9
+ * 0 0 0 0 3 0 0 8
+ * -1 0 5 0 0 0 22 0
+ * -1 8 0 0 0 0 0 0
+ * 0 0 0 0 0 10 43 0
+ * 0 0 5 0 15 0 0 0
+ * 0 0 40 0 0 0 20 0
+ *
+ * Output 1
+ * 186
+ * --------------------
+ * Input 2
+ * 7 8 50
+ * 0 0 0 0 0 0 0 9
+ * 0 0 0 0 3 0 0 8
+ * -1 0 5 0 0 0 22 0
+ * -1 8 0 0 0 0 0 0
+ * 0 0 0 0 0 10 43 0
+ * 0 0 5 0 15 0 0 0
+ * 0 0 40 0 0 0 20 0
+ *
+ * Output 2
+ * 46
+ * --------------------
+ */
+public class Boj17144 {
+
+    static final int[] dirX = {1, 0, -1, 0};
+    static final int[] dirY = {0, 1, 0, -1};
+    static int R, C, T;
+    static int AirCleanerHeadX = -1, AirCleanerHeadY = -1;
+    static int AirCleanerBodyX = -1, AirCleanerBodyY = -1;
+    static int[][] room;
+
+    public static boolean canGo(int x, int y) {
+        if(x < 0 || x > C - 1) return false;
+        if(y < 0 || y > R - 1) return false;
+        return true;
+    }
+
+    public static int[][] diffuse(int[][] src) {
+        int[][] dst = new int[R][C];
+
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                int diffuseCnt = 0;
+                int diffuseAmount = src[i][j] / 5;
+                for(int k = 0; k < 4; k++) {
+                    int nextX = j + dirX[k];
+                    int nextY = i + dirY[k];
+                    if(canGo(nextX, nextY) && src[nextY][nextX] != -1) {
+                        dst[nextY][nextX] += diffuseAmount;
+                        diffuseCnt++;
+                    }
+                }
+                dst[i][j] += src[i][j] - diffuseCnt * diffuseAmount;
+            }
+        }
+
+        return dst;
+    }
+
+    public static void rotate(int[][] src, int sx, int sy, int ex, int ey, boolean ccw) {
+        int temp;
+        int gapX = Math.abs(ex - sx);
+        int gapY = Math.abs(ey - sy);
+
+        if(ccw) {
+            temp = src[ey][sx];
+
+            // left
+            for(int i = 0; i < gapY; i++) {
+                src[ey - i][sx] = src[ey - 1 - i][sx];
+            }
+            // top
+            for(int i = 0; i < gapX; i++) {
+                src[sy][sx + i] = src[sy][sx + 1 + i];
+            }
+            // right
+            for(int i = 0; i < gapY; i++) {
+                src[sy + i][ex] = src[sy + 1 + i][ex];
+            }
+            // bottom
+            for(int i = 0; i < gapX; i++) {
+                src[ey][ex - i] = src[ey][ex - 1 - i];
+            }
+            src[ey][sx + 1] =  temp;
+
+        } else {
+            temp = src[sy][sx];
+
+            // left
+            for(int i = 0; i < gapY; i++) {
+                src[sy + i][sx] = src[sy + 1 + i][sx];
+            }
+            // bottom
+            for(int i = 0; i < gapX; i++) {
+                src[ey][sx + i] = src[ey][sx + 1 + i];
+            }
+            // right
+            for(int i = 0; i < gapY; i++) {
+                src[ey - i][ex] = src[ey - 1 - i][ex];
+            }
+            // top
+            for(int i = 0; i < gapX; i++) {
+                src[sy][ex - i] = src[sy][ex - 1 - i];
+            }
+            src[sy][sx + 1] = temp;
+        }
+    }
+
+    public static void cleanUp(int[][] src) {
+        src[AirCleanerHeadY][AirCleanerHeadX] = src[AirCleanerBodyY][AirCleanerBodyX] = 0;
+        rotate(src, 0, 0, C - 1, AirCleanerHeadY, true);
+        rotate(src, AirCleanerBodyX, AirCleanerBodyY, C - 1, R - 1, false);
+        src[AirCleanerHeadY][AirCleanerHeadX] = src[AirCleanerBodyY][AirCleanerBodyX] = -1;
+    }
+
+    public static int sum(int[][] src) {
+        int sum = 0;
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                sum += src[i][j];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void dumpRoom(int[][] src) {
+        for(int i = 0; i < R; i++) {
+            System.out.println(Arrays.toString(src[i]));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // parse first line
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        room = new int[R][C];
+
+        // parse room information
+        for(int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < C; j++) {
+                int dust = Integer.parseInt(st.nextToken());
+                room[i][j] = dust;
+                if(dust == -1 && AirCleanerHeadX == -1) {
+                    AirCleanerHeadY = i;
+                    AirCleanerBodyY = i + 1;
+                    AirCleanerHeadX = AirCleanerBodyX = j;
+                }
+            }
+        }
+
+        // calculate the dust
+        for(int i = 0; i < T; i++) {
+            room = diffuse(room);
+            cleanUp(room);
+        }
+
+        // write the answer
+        int answer = sum(room) + 2;
+        bw.write(String.valueOf(answer));
+
+        // close the buffer
+        br.close();
+        bw.close();
+    }
+}
