8f3dba2bb4b9125e5f11aed05e6ac0bf91afdad8
Lee Hong Jun <hong3883@naver.com>
2022-07-31 23:56:21 +0900

    Feat: Solved new problem BOJ 18166
    - Added solved boj problem 18166 (로봇 조립, category: disjoint-set)

---
+++ src/disjointset/BOJ18166.java
@@ -0,0 +1,224 @@
+package disjointset;
+
+import java.io.*;
+import java.util.*;
+
+
+
+/**
+ * 로봇 조립 - BOJ18166
+ * -----------------
+ * Input 1
+ * 4
+ * I 1 2
+ * I 3 2
+ * Q 1
+ * Q 4
+ *
+ * Output 1
+ * 3
+ * 1
+ * -----------------
+ */
+public class BOJ18166 {
+
+    private static class DisjointSet {
+        private int[] parents;
+        private int[] ranks;
+        private int[] nodeCounts;
+
+        public enum DumpType {
+            PARENTS,
+            RANKS,
+            NODE_COUNTS,
+            ALL
+        }
+
+        /**
+         * Constructor of DisjointSet
+         *
+         * @param n Size of DisjointSet
+         */
+        public DisjointSet(int n) {
+            parents = new int[n];   // 0 ~ n - 1
+            ranks = new int[n];
+            nodeCounts = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                nodeCounts[i] = 1;
+            }
+        }
+
+        /**
+         * Union both 'a' and 'b' nodes
+         *
+         * @param a index of 'a' node
+         * @param b index of 'b' node
+         */
+        public void union(int a, int b) {
+            // Size out of bounds
+            if (a < 0 || a > parents.length - 1) return;
+            if (b < 0 || b > parents.length - 1) return;
+
+            // Get parents
+            int aRoot = find2(a);
+            int bRoot = find2(b);
+
+            // same root
+            if (aRoot == bRoot) return;
+
+            parents[bRoot] = aRoot;
+            nodeCounts[aRoot] += nodeCounts[bRoot];
+        }
+
+        /**
+         * Find a parent of 'a' node
+         *
+         * @param a index of 'a' node
+         * @return parent of 'a' node
+         */
+        public int find(int a) {
+            if (a < 0 || a > parents.length - 1) return -1;
+            return parents[a];
+        }
+
+        /**
+         * Find a parent of 'a' node
+         * Path compression is performed with find
+         *
+         * @param a index of 'a' node
+         * @return root parent of 'a' node
+         */
+        public int find2(int a) {
+            if (a < 0 || a > parents.length - 1) return -1;
+            if (a == parents[a]) return a;
+            else return parents[a] = find2(parents[a]);  // path compression
+        }
+
+        /**
+         * Compare parent of both 'a' and 'b' nodes
+         *
+         * @param a index of 'a' node
+         * @param b index of 'b' node
+         * @return
+         */
+        public boolean compareParent(int a, int b) {
+            return find(a) == find(b);
+        }
+
+        /**
+         * Compare parent of both 'a' and 'b' nodes
+         * Path compression is performed with find
+         *
+         * @param a index of 'a' node
+         * @param b index of 'b' node
+         * @return
+         */
+        public boolean compareParent2(int a, int b) {
+            return find2(a) == find2(b);
+        }
+
+        /**
+         * Get node count of children
+         *
+         * @param a index of 'a' node
+         * @return
+         */
+        public int getNodeCount(int a) {
+            return nodeCounts[a];
+        }
+
+        /**
+         * Size of DisjointSet (Node count of DisjointSet)
+         *
+         * @return
+         */
+        public int size() {
+            return parents.length;
+        }
+
+        /**
+         * Print information of DisjointSet
+         *
+         * @param type
+         */
+        public void dump(DisjointSet.DumpType type) {
+            StringBuilder sb = new StringBuilder();
+
+            switch (type) {
+                case PARENTS:
+                    sb.append("Parents: ");
+                    sb.append(Arrays.toString(parents));
+                    break;
+
+                case RANKS:
+                    sb.append("Ranks: ");
+                    sb.append(Arrays.toString(ranks));
+                    break;
+
+                case NODE_COUNTS:
+                    sb.append("Node counts: ");
+                    sb.append(Arrays.toString(nodeCounts));
+                    break;
+
+                case ALL:
+                    dump(DisjointSet.DumpType.PARENTS);
+                    dump(DisjointSet.DumpType.RANKS);
+                    dump(DisjointSet.DumpType.NODE_COUNTS);
+                    break;
+            }
+
+            System.out.println(sb);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("DisjointSet{");
+            sb.append("parents=");
+            sb.append(Arrays.toString(parents));
+            sb.append('}');
+
+            return sb.toString();
+        }
+    }
+
+    static final int SIZE = (int) 1e6;
+    static int N;
+    static DisjointSet dSet;
+    static StringBuilder result = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        // Input & Output stream
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+        dSet = new DisjointSet(SIZE + 1);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            char inst = st.nextToken().charAt(0);
+            switch (inst) {
+                case 'I':
+                    int a = Integer.parseInt(st.nextToken());
+                    int b = Integer.parseInt(st.nextToken());
+                    dSet.union(a, b);
+                    break;
+
+                case 'Q':
+                    int c = Integer.parseInt(st.nextToken());
+                    result.append(dSet.nodeCounts[dSet.find2(c)]).append('\n');
+                    break;
+            }
+        }
+
+        // write the result
+        bw.write(result.toString());
+
+        // close the buffer
+        br.close();
+        bw.close();
+    }
+}
